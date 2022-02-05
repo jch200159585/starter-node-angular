981e86dbe90b7f37b7bd6bffcde0736df492f877
josh herrera <jherrera1013@gmail.com>
2022-02-05 17:57:11 -0500

    test deployment

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.239.111.114',
+      host: '3.85.20.255',
       username: 'ubuntu',
       privateKey: 'labsuser.pem'
     })
